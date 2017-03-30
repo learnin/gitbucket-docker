412992638d6f57b8815f611e744ac81c102eea84
learnin <i.learnin@gmail.com>
2017-03-30 15:50:20 +0900

    Change docker run restart option

--- run_for_32bit_os.sh
+++ run_for_32bit_os.sh
@@ -14,7 +14,7 @@ docker container run \
   -e 'TZ=Asia/Tokyo' \
   --name gitbucketdocker_db_1 \
   -p 50103:3306 \
-  --restart always \
+  --restart unless-stopped \
   -v /etc/localtime:/etc/localtime:ro \
   -v gitbucketdocker_db-data:/var/lib/mysql \
   gitbucketdocker_db
@@ -36,7 +36,7 @@ docker container run \
   --name gitbucketdocker_app_1 \
   -p 50003:80 \
   -p 29418:29418 \
-  --restart always \
+  --restart unless-stopped \
   -v /etc/localtime:/etc/localtime:ro \
   -v gitbucketdocker_app-data:/gitbucket \
   gitbucketdocker_app
